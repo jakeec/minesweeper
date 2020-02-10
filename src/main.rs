afde269d8120c6f3ac69b11d167cdbfb36c48384
Jake Carrington <jake.ethan.carrington@gmail.com>
2020-02-10 16:25:08 +0000

    Starting to add difficulty setting

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 mod minesweeper;
-use minesweeper::{GameState, Minesweeper};
+use minesweeper::{Difficulty, GameState, Minesweeper};
 use std::io::Stdin;
 
 fn main() {
-    let mut game = Minesweeper::new();
+    let stdin = std::io::stdin();
+    println!("Select difficulty: ");
+    let mut difficulty = Difficulty::Easy;
+    let mut game = Minesweeper::new(difficulty);
     game.print();
     let mut input: String = String::from("");
+    let mut stdout = std::io::stdout();
     while game.game_state == GameState::Playing {
-        let stdin = std::io::stdin();
         stdin.read_line(&mut input);
         game.parse_move(&input);
         input = String::from("");
--- src/minesweeper.rs
+++ src/minesweeper.rs
@@ -33,27 +33,54 @@ pub struct Minesweeper {
     pub grid: Grid,
 }
 
+struct GridDimensions {
+    x: usize,
+    y: usize,
+}
+
+impl GridDimensions {
+    pub fn new(x: usize, y: usize) -> Self {
+        Self { x: x, y: y }
+    }
+}
+
+pub enum Difficulty {
+    Easy,
+    Medium,
+    Hard,
+    Custom(GridDimensions),
+}
+
+fn create_grid(dimensions: GridDimensions) -> Grid {
+    let mut grid: Grid = Vec::new();
+    for x in 0..dimensions.x {
+        let mut row: Row = Vec::new();
+        for y in 0..dimensions.y {
+            row.push(Square::new(false, SquareState::Clear));
+        }
+
+        grid.push(row);
+    }
+
+    grid
+}
+
+fn clear_screen() {
+    print!("{}[2J", 27 as char);
+}
+
 impl Minesweeper {
-    pub fn new() -> Self {
+    pub fn new(difficulty: Difficulty) -> Self {
         Self {
             game_state: GameState::Playing,
-            grid: vec![
-                vec![
-                    Square::new(false, SquareState::Bomb),
-                    Square::new(false, SquareState::Clear),
-                    Square::new(false, SquareState::Clear),
-                ],
-                vec![
-                    Square::new(false, SquareState::Bomb),
-                    Square::new(false, SquareState::Clear),
-                    Square::new(false, SquareState::Clear),
-                ],
-                vec![
-                    Square::new(false, SquareState::Bomb),
-                    Square::new(false, SquareState::Clear),
-                    Square::new(false, SquareState::Clear),
-                ],
-            ],
+            grid: match difficulty {
+                Difficulty::Easy => create_grid(GridDimensions::new(5, 5)),
+                Difficulty::Medium => create_grid(GridDimensions::new(7, 7)),
+                Difficulty::Hard => create_grid(GridDimensions::new(12, 12)),
+                Difficulty::Custom(GridDimensions { x, y }) => {
+                    create_grid(GridDimensions::new(x, y))
+                }
+            },
         }
     }
 
@@ -81,10 +108,20 @@ impl Minesweeper {
 
     pub fn parse_move(&mut self, input: &str) {
         let dims: Vec<&str> = input.split(" ").collect();
-        let x = dims[0].parse::<usize>().unwrap();
-        let y = dims[1].replace("\n", "").parse::<usize>().unwrap();
+        let x = match dims[0].replace("-", "").parse::<usize>() {
+            Err(err) => return,
+            Ok(x) => x,
+        };
+        let y = match dims[1].replace("-", "").replace("\n", "").parse::<usize>() {
+            Err(err) => return,
+            Ok(x) => x,
+        };
 
-        self.grid[x][y].revealed = true;
-        self.print();
+        if x >= self.grid.len() || y >= self.grid[0].len() {
+        } else {
+            self.grid[x][y].revealed = true;
+            clear_screen();
+            self.print();
+        }
     }
 }
